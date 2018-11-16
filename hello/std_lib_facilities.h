e7d9e85b15ed437728d12a4e68bbf3f1532d4177
Fotis Moulatsiotis <fotis.moulatsiotis@gmail.com>
2018-11-17 01:02:09 +0200

    first commit

---
+++ hello/std_lib_facilities.h
@@ -0,0 +1,9 @@
+//#pragma once
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+inline void keep_window_open () { char ch; cin >> ch; }
